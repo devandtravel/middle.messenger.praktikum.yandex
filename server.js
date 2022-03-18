16abbf1267087d9e2171482cefdbe161cbab2092
devandtravel <devandtravel@gmail.com>
2022-03-18 18:08:00 +0300

    simple template

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const __dirname = dirname(__filename)
 const app = express()
 const PORT = 3000
 
-app.use(express.static(path.join(__dirname + '/dist/')))
+app.use(express.static(path.join(__dirname + '/dist')))
 
 app.listen(PORT, function () {
   console.log(`Example app listening on port ${PORT}!`)
