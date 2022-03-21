e6272f14ae5c1f200e0fc78d013c9a6da4083fa2
devandtravel <devandtravel@gmail.com>
2022-03-22 01:42:19 +0300

    styles

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path')
 const app = express()
 const PORT = 3000
 
-app.use(express.static(path.join(__dirname + '/dist')))
+app.use(express.static(path.join(__dirname + '/build')))
 
 app.listen(PORT, function () {
   console.log(`Example app listening on port ${PORT}!`)
