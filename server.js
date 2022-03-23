b21c2505dace9424639219ce9d24773d2326ba15
devandtravel <devandtravel@gmail.com>
2022-03-23 21:59:58 +0300

    fix favicon, remove build folder

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ dotenv.config()
 const app = express()
 const DEFAULT_PORT = 3000
 
-app.use(express.static(path.join(__dirname + '/build')))
+app.use(express.static(path.join(__dirname + '/dist')))
 
 const PORT = env.PORT || DEFAULT_PORT
 
--- src/index.js
+++
@@ -1,6 +0,0 @@
-import { sum } from './modules/sum'
-
-const root = document.querySelector('#root')
-root.textContent = sum(6, -1).toString()
-
-// import template from './template.hbs'
